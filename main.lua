32671ad4c5bea742a5dc60b0e23c2f6c909d44e8
Charles Holbrow <charlesholbrow@gmail.com>
2012-03-15 18:45:22 -0400

    organizing!

--- main.lua
+++ main.lua
@@ -1,59 +1,34 @@
 --==============================================================
--- set up
+-- init some global settings
 --==============================================================
 X_SIZE = 640
-Y_SIZE = 420
+Y_SIZE = 640
 
 UP_ARROW = 357
 DOWN_ARROW = 359
 LEFT_ARROW = 356
 RIGHT_ARROW = 358
 print ( 'LUA_PATH: ' .. package.path )
-MOAISim.openWindow("Fast Game", X_SIZE, Y_SIZE)
-
-viewport = MOAIViewport.new ()
-viewport:setScale ( X_SIZE, Y_SIZE )
-viewport:setSize ( X_SIZE, Y_SIZE )
-
-layer = MOAILayer2D.new ()
-layer:setViewport ( viewport )
-MOAISim.pushRenderPass ( layer )
-
-d1 = MOAIGfxQuad2D.new ()
-d1:setTexture ( "img/openlobster.png" )
-d1:setRect ( -128, -128, 128, 128 )-- Size of tiles in world space
-
-deckCharacters = MOAITileDeck2D.new ()
-deckCharacters:setTexture ( "img/man_map_4x1.png" )
-deckCharacters:setSize (4, 1)
-deckCharacters:setRect ( -28, -40, 28, 40 )-- Size of tiles in world space
 
 
+-- could use load file instead of require, 
+-- in that case, I wouln't need to init global variables in another file
+require 'lua/setupView'
+
+mainDecks = {}
+require 'lua/setupDecks'
+
+require 'lua/setupInput'
+
+-- TODO: create a table of props. update setupInput.lua
 p1 = MOAIProp2D.new ()
-p1:setDeck ( deckCharacters )
+p1:setDeck ( mainDecks.characters )
 p1:setIndex ( 1 )
 p1:setLoc ( 0, -20 )
-layer:insertProp ( p1 )
+mainLayer:insertProp ( p1 )
 
-function onKeyboard ( key, down)
-   print ('key:  ' .. tostring ( key ) )
-   print ('down: ' .. tostring ( down ) )
-end
-MOAIInputMgr.device.keyboard:setCallback( onKeyboard )
-
-function onMouseLeftEvent ( down )
-   -- Mouse left press
-   if down == true then
-      p1:setIndex( ( p1:getIndex () % 3 ) + 1 ) 
-      print ( p1:getIndex() )
-   -- Mouse right press
-   else
-
-   end
-end
-MOAIInputMgr.device.mouseLeft:setCallback ( onMouseLeftEvent ) 
-
-charcodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,:;!?()&/-'
+-- TODO: make font and textbox local somehow 
+local charcodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,:;!?()&/-'
 font = MOAIFont.new ()
 font:loadFromTTF ( 'fonts/arialbd.ttf', charcodes, 16, 163 )
 
@@ -63,20 +38,13 @@ textbox:setTextSize ( font:getScale () )
 textbox:setRect ( -50, -50, 50, 50 )
 textbox:setYFlip ( true )
 
-function onPointerEvent ( x, y )
-   mx, my = layer:wndToWorld ( x, y )
-   textbox:setLoc ( mx, my ) 
-   textbox:setString ( tostring ( 'X' .. mx .. '\n' .. 'Y' .. my ) )
-   layer:insertProp ( textbox )
-   -- print ( 'Pointer:', x, y )
-end
-MOAIInputMgr.device.pointer:setCallback ( onPointerEvent )
 
+-- TODO: move more of the movement stuff into setupInput
 mainRoutine = MOAICoroutine.new ()
 mainRoutine:run (
    function ()
       local run = true
-      playerSpeed = 50
+      playerSpeed = 75 
       while run do
 
          local dt = coroutine.yield ()
