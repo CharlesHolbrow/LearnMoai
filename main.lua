524d7217c27d5948d131eb9a07597f58e45f5016
Charles Holbrow <charlesholbrow@gmail.com>
2012-03-19 00:42:41 -0400

    adding tile map to demo project

--- main.lua
+++ main.lua
@@ -1,8 +1,29 @@
+--[[
+Basic data structures to be abstracted. 
+Represent each with no more than one global variable
+
+Input 
+   -- What do we do with input?
+   -- Will eventually need windows, focus
+
+
+World Building 
+   -- Load decks 
+   -- Create props
+
+   -- Tile Object - Base class for terrain, character, 
+   -- Location object - could be many layers of Tiles on a location
+   -- Map object - maps are made of many locations
+
+   -- Save and load maps 
+
+]]
+
 --==============================================================
 -- init some global settings
 --==============================================================
-X_SIZE = 640
-Y_SIZE = 640
+X_SIZE = 900
+Y_SIZE = 760
 
 UP_ARROW = 357
 DOWN_ARROW = 359
@@ -20,11 +41,35 @@ require 'lua/setupDecks'
 
 require 'lua/setupInput'
 
+-- Create the tile grid
+grid = MOAIGrid.new ()
+grid:initHexGrid ( 4, 12, 64 )
+grid:setRepeat ( false )
+
+for y = 1, 12 do
+   for x = 1, 4 do
+      grid:setTile ( x, y, (x * 2 ) - ( y % 2 ) )
+   end
+end
+
+tileDeck = MOAITileDeck2D.new ()
+tileDeck:setTexture ( "img/terrain4x4.png" )
+tileDeck:setSize ( 4, 4, 0.25, 0.216796875 )
+
+prop = MOAIProp2D.new ()
+prop:setDeck ( tileDeck )
+prop:setGrid ( grid )
+prop:setLoc ( 0, 0 )
+prop:setLoc ( -440, -330 )
+--prop:forceUpdate ()
+mainLayer:insertProp ( prop )
+
+-- Create player prop
 -- TODO: create a table of props. update setupInput.lua
 p1 = MOAIProp2D.new ()
 p1:setDeck ( mainDecks.characters )
 p1:setIndex ( 1 )
-p1:setLoc ( 0, -20 )
+p1:setLoc ( 0, 0 )
 mainLayer:insertProp ( p1 )
 
 -- TODO: make font and textbox local somehow 
